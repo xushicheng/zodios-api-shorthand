b7e24c890396347703e36919841d3b13e66e132e
xushicheng <randaoshengxia@gmail.com>
2024-11-13 18:22:40 +0800

    feat: 修改项目, 去掉了空格做匹配

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,58 +5,58 @@ import { Zodios, makeApi } from "@zodios/core";
 import { zodiosContext } from "@zodios/express";
 
 const endpoints = api({
-	"POST transactions": {
-		path: "/v1/transactions",
-		queries: {
-			limit: z.number(),
-			offset: z.number(),
-		},
-		body: z.string(),
-		response: z.number(),
-		description: {
-			path: "Create a transaction",
-			response: "Returns the Transaction ID",
-			queries: {
-				limit: "Add a limit to pagination",
-				offset: "Page number",
-			},
-		},
-	},
-	"GET transaction": {
-		path: "/v1/transaction/:id",
-		params: {
-			id: z.number(),
-		},
-		response: z.number(),
-		errors: {
-			"404": z.literal("Not Found"),
-		},
-		description: {
-			path: "Find a transaction by ID",
-			response: "Returns the transaction ID",
-			errors: {
-				"404": "This error is triggered when the transaction is not found in the database",
-			},
-		},
-	},
+    "postTransactions": {
+        path: "/v1/transactions",
+        queries: {
+            limit: z.number(),
+            offset: z.number(),
+        },
+        body: z.string(),
+        response: z.number(),
+        description: {
+            path: "Create a transaction",
+            response: "Returns the Transaction ID",
+            queries: {
+                limit: "Add a limit to pagination",
+                offset: "Page number",
+            },
+        },
+    },
+    "getTransaction": {
+        path: "/v1/transaction/:id",
+        params: {
+            id: z.number(),
+        },
+        response: z.number(),
+        errors: {
+            "404": z.literal("Not Found"),
+        },
+        description: {
+            path: "Find a transaction by ID",
+            response: "Returns the transaction ID",
+            errors: {
+                "404": "This error is triggered when the transaction is not found in the database",
+            },
+        },
+    },
 });
 
 const ctx = zodiosContext(
-	z.object({
-		foo: z.string(),
-	}),
+    z.object({
+        foo: z.string(),
+    }),
 );
 const app = ctx.app(makeApi(endpoints));
 app.get("/v1/transaction/:id", (req, _) => {
-	req.params.id;
+    req.params.id;
 });
 app.post("/v1/transactions", (req, _) => {
-	req.query.limit;
-	req.query.offset;
+    req.query.limit;
+    req.query.offset;
 });
 
 it("Has the runtime API methods", async () => {
-	const API = new Zodios("http://foo.bar.com", makeApi(endpoints));
-	expect(API.postTransactions).toBeDefined();
-	expect(API.getTransaction).toBeDefined();
+    const API = new Zodios("http://foo.bar.com", makeApi(endpoints));
+    expect(API.postTransactions).toBeDefined();
+    expect(API.getTransaction).toBeDefined();
 });
